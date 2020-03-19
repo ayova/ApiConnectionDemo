f0d7a68eb3c248f9cd76a32fa1b661979de9eab7
Agu <agustin.ovari@gmail.com>
2020-03-19 19:12:12 +0100

    Initial Commit

---
+++ ApiConnectionDemoTests/ApiConnectionDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ApiConnectionDemoTests.swift
+//  ApiConnectionDemoTests
+//
+//  Created by ADMIN on 19/03/2020.
+//  Copyright © 2020 ADMIN. All rights reserved.
+//
+
+import XCTest
+@testable import ApiConnectionDemo
+
+class ApiConnectionDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
